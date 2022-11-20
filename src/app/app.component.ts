6d312203e45f5ace537c12267c4a52f54e76b68a
aDNpz <74315928+aDNpz@users.noreply.github.com>
2022-11-20 17:37:09 +0100

    Hausuebung 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+  cnt=0;
+  total=0;
+  final:any;
+  ctrlrating = new FormControl(0);
+
+  GetStars(){
+    console.log(this.ctrlrating.value);
+    this.cnt++;
+    this.total +=this.ctrlrating?.value || 0;
+    this.final = (this.total/this.cnt).toFixed(2);
+  }
+
   title = 'starrating';
 }
